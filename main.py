d936cdf9dae87e3ba58140230c459b81e745f4f9
Josh Callsen <jcallsen@vt.edu>
2021-03-27 16:36:43 -0400

    Monthly endpoint

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-from pydantic import BaseModel
-from fastapi import FastAPI
+from typing import List
+from fastapi import FastAPI, Query
 import numpy_financial as npf
 import numpy as np
 from datetime import date
@@ -11,11 +11,23 @@ getfired = FastAPI()
 
 @getfired.get('/')
 async def root():
-    return( {'message': 'Hello World'})
+    return( {'message': 'Please use an end point'})
 
-#monthly_rate = 12 * ((1 + (real_rate)) ** (1. / 12) - 1)
 
-@getfired.get('/budget')
+@getfired.get('/month')
+async def eval_budget(income: float, assets:float, retirement_expenses: float, expenses: List[float] = Query([]), categories: List[str] = Query([]), withdraw: float = 0.04, real_rate: float = 0.06):
+    monthly_rate = 12 * ((1 + (real_rate)) ** (1. / 12) - 1)
+    result = float(npf.nper(monthly_rate, -(income - sum(expenses)), -assets, retirement_expenses / withdraw))
+
+    time = {}
+    for cat in categories:
+        new_expenses = sum(expenses) - expenses[categories.index(cat)] 
+        time[cat] = result - float(npf.nper(monthly_rate, -(income - new_expenses), -assets, retirement_expenses / withdraw))
+
+    return({'months left' : result, 'categories' : time})
+
+
+@getfired.get('/annual')
 async def eval_budget(income: float, assets: float, curr_expenses: float, retirement_expenses: float, withdraw: float = 0.04, real_rate: float = 0.06):
     result = npf.nper(real_rate, -(income - curr_expenses), -assets, retirement_expenses / withdraw) * 12
     result_year = ceil(result / 12) 
@@ -26,6 +38,4 @@ async def eval_budget(income: float, assets: float, curr_expenses: float, retire
     graph[0] = [curr_year + i for i in range(result_year)]
     graph[1] = npf.fv(real_rate, np.linspace(1, result_year, result_year), -(income - curr_expenses), -assets)
 
-    print(graph)
-
-    return( {'months left': result, 'graph' : graph.tolist()} )
+    return({'months left': result, 'graph' : graph.tolist()})
